7cfb5d661e97109f286b56b66e5b14088e8e7a29
Andrew Zhou <andrewzhou16@gmail.com>
2025-01-18 22:57:06 -0800

    Add: Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import clsx from "clsx";
+
 export default function Home() {
-  return null;
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center gap-3">
+      <span className="font-bold mb-3 text-xl">THOSE WHO KNOW</span>
+      <input
+        className={clsx(
+          "w-64 h-12 rounded-full border-2 border-neutral-200 text-center font-medium",
+          "focus:border-purple-600 focus:outline-none focus:border-2"
+        )}
+        placeholder="Room Code"
+      />
+      <span className="font-medium text-sm text-neutral-300">OR</span>
+      <button className="w-64 h-12 rounded-full bg-neutral-900 text-white font-medium">
+        Create Room
+      </button>
+    </div>
+  );
 }
