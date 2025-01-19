3c1aeca12fded5988b5eb5c85afaa4f6bcf6181b
Andrew Zhou <andrewzhou16@gmail.com>
2025-01-19 00:12:24 -0800

    Add: Dynamic header

--- src/app/[id]/(authorized)/(header)/layout.tsx
+++ src/app/[id]/(authorized)/(header)/layout.tsx
@@ -1,12 +1,30 @@
+"use client";
+
+import { useSelectedLayoutSegment } from "next/navigation";
+
 const Header = () => {
+  const path = useSelectedLayoutSegment();
+
+  let centerComponent;
+  switch (path) {
+    case "lobby":
+      centerComponent = <span className="font-semibold">Lobby</span>;
+      break;
+    default:
+      centerComponent = (
+        <span>
+          The question topic is{" "}
+          <span className="font-semibold">{"Sports"}</span>
+        </span>
+      );
+  }
+
   return (
     <div className="w-full h-16 flex items-center px-5 border-b-2 border-neutral-100">
       <span className="flex-1">
         You&apos;re <span className="font-semibold">andou</span>
       </span>
-      <span className="flex-1 text-center">
-        <span className="font-semibold">Yuchen</span> has joined
-      </span>
+      <span className="flex-1 text-center">{centerComponent}</span>
       <span className="flex-1 text-end">
         Room <span className="font-semibold">AB912K</span>
       </span>
