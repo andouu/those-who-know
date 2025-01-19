84896019f4ef4b992e5c774ebeb58587eeef6eb4
Andrew Zhou <andrewzhou16@gmail.com>
2025-01-19 00:53:04 -0800

    Add: Form wrap room code input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
+"use client";
 import clsx from "clsx";
 
 export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center gap-3">
       <span className="font-bold mb-3 text-xl">THOSE WHO KNOW</span>
-      <input
-        className={clsx(
-          "w-64 h-12 rounded-full border-2 border-neutral-200 text-center font-medium",
-          "focus:border-purple-600 focus:outline-none focus:border-2"
-        )}
-        placeholder="Room Code"
-      />
+      <form
+        className="flex items-center gap-3"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <input
+          className={clsx(
+            "w-64 h-12 rounded-full border-2 border-neutral-200 text-center font-medium",
+            "focus:border-purple-600 focus:outline-none focus:border-2"
+          )}
+          placeholder="Room Code"
+        />
+      </form>
       <span className="font-medium text-sm text-neutral-300">OR</span>
       <button className="w-64 h-12 rounded-full bg-neutral-900 text-white font-medium">
         Create Room
